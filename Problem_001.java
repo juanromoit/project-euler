cd7ee615a20854091ea30ef336deee5746d1cd31
Juan Romo <juanromoit@gmail.com>
2019-03-15 10:12:49 -0500

    Add problem 1

---
+++ Problem_001.java
@@ -0,0 +1,11 @@
+class Problem_001 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 1_000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
