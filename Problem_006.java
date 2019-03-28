4a6cee5d5edf2c2ed1c6e4d2619cfc8b2e541811
Juan Romo <juanromoit@gmail.com>
2019-03-28 12:13:44 -0500

    Refactor problem 6 loop variable

--- Problem_006.java
+++ Problem_006.java
@@ -2,9 +2,9 @@ class Problem_006 {
     public static void main(String[] args) {
         int sumOfSquares = 0;
         int squareOfSum = 0;
-        for (int i = 0; i <= 100; i++) {
-            sumOfSquares += i * i;
-            squareOfSum += i;
+        for (int naturalNumber = 1; naturalNumber <= 100; naturalNumber++) {
+            sumOfSquares += naturalNumber * naturalNumber;
+            squareOfSum += naturalNumber;
         }
         squareOfSum *= squareOfSum;
         System.out.println(squareOfSum - sumOfSquares);
