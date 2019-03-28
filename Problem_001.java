0b3f3134f0da1c5cc5cb65ea64847051599b7aa6
Juan Romo <juanromoit@gmail.com>
2019-03-28 12:05:39 -0500

    Refactor problem 1 loop variable

--- Problem_001.java
+++ Problem_001.java
@@ -1,9 +1,9 @@
 class Problem_001 {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 0; i < 1_000; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
-                sum += i;
+        for (int dividend = 1; dividend < 1_000; dividend++) {
+            if (dividend % 3 == 0 || dividend % 5 == 0) {
+                sum += dividend;
             }
         }
         System.out.println(sum);
